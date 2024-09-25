549570c1f5eb7de20c78fa92c50a46dda4d049bd
Sagor Hossain <sagor.cnits@gmail.com>
2024-09-25 12:39:12 +0600

    solve different user

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,75 +1,135 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoSendOutline } from "react-icons/io5";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000/");
 
 const Home = () => {
-  const [message, setMessage] = useState("");
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [userName, setUserName] = useState("");
+  const [chatActive, setChatActive] = useState(false);
+
+  // useEffect
+
+  useEffect(() => {
+    socket.on("welcome", (message) => {
+      console.log(message, "welcome message");
+    });
+
+    socket.on("recived-message", (message) => {
+      setMessages((prevMessage) => [...prevMessage, message]);
+    });
+
+    return () => {
+      socket.off("welcome");
+      socket.off("recived-message");
+    };
+  }, []);
 
   const handleMessage = (data) => {
     data.preventDefault();
-    console.log(message);
-    setMessage(" ");
+    const messageData = {
+      message: newMessage,
+      user: userName,
+    };
+
+    socket.emit("send-message", messageData);
+    setNewMessage(" ");
   };
 
-  // console.log(message);
+  console.log(messages);
 
   return (
-    <main className="flex">
-      <div className="w-[25%] bg-[#2C2C2C] h-screen border-r border-[#222222] p-3">
-        <div className="pb-4 px-2">
-          <input
-            type="text"
-            placeholder="search or start new chat"
-            className="w-full bg-[#454545] py-3 px-4 rounded-md focus:outline-none text-white"
-          />
-        </div>
-        <div className="user_chat overflow-auto px-2">
-          {[1, 2, 3, 4, , 5, 6, 7, 8, 1, 2, 3, 4, , 5, 6, 7, 8].map(
-            (item, id) => (
-              <UserCard key={id}></UserCard>
-            )
-          )}
-        </div>
-      </div>
-
-      <section className="w-[75%] bg-[#2C2C2C] h-screen relative">
-        <nav className="px-3 py-2 border-b border-[#222222]">
-          <div className="flex gap-3 items-center">
-            <div className="size-12 rounded-full overflow-hidden">
-              <img
-                src="https://lh3.googleusercontent.com/a/ACg8ocL-G38YycrNTgadRSctDVoHou9KPcM8OrqlQk9-I03rsqVALHA=s288-c-no"
-                alt="user_img"
+    <>
+      {chatActive ? (
+        <main className="flex">
+          <div className="w-[25%] bg-[#2C2C2C] h-screen border-r border-[#222222] p-3">
+            <div className="pb-4 px-2">
+              <input
+                type="text"
+                placeholder="search or start new chat"
+                className="w-full bg-[#454545] py-3 px-4 rounded-md focus:outline-none text-white"
               />
             </div>
-            <p className="font-bold text-white">Sagor Hossain</p>
+            <div className="user_chat overflow-auto px-2">
+              {[1, 2, 3, 4, , 5, 6, 7, 8, 1, 2, 3, 4, , 5, 6, 7, 8].map(
+                (item, id) => (
+                  <UserCard key={id}></UserCard>
+                )
+              )}
+            </div>
           </div>
-          <div>{/* <MdCall></MdCall> */}</div>
-        </nav>
-        <div className="user_chat bg-[url('https://img.freepik.com/free-photo/simple-tree-sketch-falling-leaves_1379-348.jpg?t=st=1727168902~exp=1727172502~hmac=38dfee6df293a36e0a67d7325aaf7d9f5bd4099f97d05e37be6524f2463bf01e&w=740')] bg-no-repeat bg-cover px-8">
-          <div className="flex justify-end pt-4">
-            <p className="w-[300px]  p-4 bg-[#005C4B] rounded-md text-white">Hello There</p>
-          </div>
-        </div>
 
-        <div className="border-t border-[#222222] absolute bottom-0 left-0 right-0 bg-[#2c2c2c] ">
-          <form
-            onSubmit={handleMessage}
-            className="flex items-center justify-between px-10"
+          <section className="w-[75%] bg-[#2C2C2C] h-screen relative">
+            <nav className="px-3 py-2 border-b border-[#222222]">
+              <div className="flex gap-3 items-center">
+                <div className="size-12 rounded-full overflow-hidden">
+                  <img
+                    src="https://lh3.googleusercontent.com/a/ACg8ocL-G38YycrNTgadRSctDVoHou9KPcM8OrqlQk9-I03rsqVALHA=s288-c-no"
+                    alt="user_img"
+                  />
+                </div>
+                <p className="font-bold text-white">Sagor Hossain</p>
+              </div>
+              <div>{/* <MdCall></MdCall> */}</div>
+            </nav>
+            <div className="user_message bg-[url('https://img.freepik.com/free-photo/simple-tree-sketch-falling-leaves_1379-348.jpg?t=st=1727168902~exp=1727172502~hmac=38dfee6df293a36e0a67d7325aaf7d9f5bd4099f97d05e37be6524f2463bf01e&w=740')] bg-no-repeat bg-cover px-8 overflow-auto">
+              {messages.length > 0 &&
+                messages.map((message, id) => {
+                  return (
+                    <div key={id} className={`${userName === message.user ? "ml-auto" : ""} w-[300px]`}>
+                      <div>
+                        <p className="text-white font-bold">{message.user}</p>
+                      </div>
+                      <div className="  p-4 bg-[#005C4B] rounded-md text-white">
+                        <p>{message?.message}</p>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+
+            <div className="border-t border-[#222222] absolute bottom-0 left-0 right-0 bg-[#2c2c2c] ">
+              <form
+                onSubmit={handleMessage}
+                className="flex items-center justify-between px-10"
+              >
+                <input
+                  type="text"
+                  placeholder="text"
+                  name="message"
+                  onChange={(e) => setNewMessage(e.target.value)}
+                  value={newMessage}
+                  className="py-4 bg-[#2c2c2c] w-full focus:outline-none text-white"
+                />
+                <button type="submit" className="text-white">
+                  <IoSendOutline className="cursor-pointer"></IoSendOutline>
+                </button>
+              </form>
+            </div>
+          </section>
+        </main>
+      ) : (
+        <form className="w-1/3 mx-auto mt-10">
+          <input
+            className="focus:outline-none border px-3 py-2"
+            type="text"
+            placeholder="userName"
+            name="userName"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            className="ml-3 px-3 py-2 cursor-pointer bg-green-400"
+            onClick={() => setChatActive(true)}
           >
-            <input
-              type="text"
-              placeholder="text"
-              name="message"
-              onChange={(e) => setMessage(e.target.value)}
-              value={message}
-              className="py-4 bg-[#2c2c2c] w-full focus:outline-none text-white"
-            />
-            <button type="submit" className="text-white">
-              <IoSendOutline className="cursor-pointer"></IoSendOutline>
-            </button>
-          </form>
-        </div>
-      </section>
-    </main>
+            login
+          </button>
+        </form>
+      )}
+    </>
   );
 };
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
+  const handleUserName = (data) => {
+    data.preventDefault();
+    const userName = data.target.userName.value;
+    const userId = data.target.userId.value;
+
+    // localStorage.setItem("user", JSON.stringify({ userName, userId }));
+  };
+
+  return (
+    <form onSubmit={handleUserName} className="w-1/3 mx-auto mt-10">
+      <input
+        className="focus:outline-none border px-3 py-2"
+        type="text"
+        placeholder="userName"
+        name="userName"
+      />
+      <input
+        className="focus:outline-none border px-3 py-2"
+        type="text"
+        placeholder="userId"
+        name="userId"
+      />
+      <button type="submit" className="ml-3 px-3 py-2 cursor-pointer bg-green-400">
+        login
+      </button>
+    </form>
+  );
 };
 
 export default Login;
